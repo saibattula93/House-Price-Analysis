2845b13c8ecbab5034fcc2bbc43c7cbe6905cbc0
saibattula93 <saidurgaprasadbattula@gmail.com>
2023-09-27 10:49:59 +0530

    update predict pipeline

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import pandas as pd
 from src.exception import CustomException
@@ -10,16 +11,29 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
-            model_path='C:\House_price\artifacts\model.pkl'
-            preprocessor_path='artifacts\preprocessor.pkl'
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            print("Before Loading")
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
+            print("After Loading")
             data_scaled=preprocessor.transform(features)
             preds=model.predict(data_scaled)
             return preds
         
         except Exception as e:
             raise CustomException(e,sys)
+        # try:
+        #     model_path='C:\House_price\artifacts\model.pkl'
+        #     preprocessor_path='artifacts\preprocessor.pkl'
+        #     model=load_object(file_path=model_path)
+        #     preprocessor=load_object(file_path=preprocessor_path)
+        #     data_scaled=preprocessor.transform(features)
+        #     preds=model.predict(data_scaled)
+        #     return preds
+        
+        # except Exception as e:
+        #     raise CustomException(e,sys)
 
 
 
